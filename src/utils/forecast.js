883f5d6f6f3ab52acccec65098389aa7082110fa
Youssef <ahmed.youssef1@ibm.com>
2021-04-28 21:32:23 +0200

    Add humidity to forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,8 +2,8 @@ const request = require("postman-request");
 
 const forecast = (lang, long, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=0a1fae43bb6e64fbf67bc314e12f169b&query=${lang},${long}&units=f`;
-  
-  request({ url, json: true }, (error, {body}) => {
+
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       return callback("Unable to connect to weather service!", undefined);
     }
@@ -12,8 +12,14 @@ const forecast = (lang, long, callback) => {
       return callback("Unable to find location", undefined);
     }
 
-    const { temperature, feelslike, weather_descriptions } = body.current;
-    const forecast = `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It's feels like ${feelslike} degrees out.`;
+    const {
+      temperature,
+      feelslike,
+      weather_descriptions,
+      humidity,
+    } = body.current;
+
+    const forecast = `${weather_descriptions[0]}. It is currently ${temperature} degrees out. It's feels like ${feelslike} degrees out. The humidity is ${humidity}.`;
     callback(undefined, forecast);
   });
 };
